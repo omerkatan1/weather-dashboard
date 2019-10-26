bcb28da3a4855c3fdb901f77eddcb30b1fee5f4d
Omer Katan <omerkatan@live.com>
2019-10-26 01:41:57 -0700

    finishing up the local Storage buttons

--- Assets/logic.js
+++ Assets/logic.js
@@ -1,5 +1,5 @@
 
-function generateAllWeather(currentWeatherURL, localSavedBtns, savedBtns) {
+function generateAllWeather(currentWeatherURL) {
     $.ajax({
         url: currentWeatherURL,
         method: "GET"
@@ -7,41 +7,15 @@ function generateAllWeather(currentWeatherURL, localSavedBtns, savedBtns) {
         console.log(response);
 
 
-        // // this makes it so the user cant get the same saved button twice
-        // var inLocalArray;
-        // var inArray;
 
-        // // checks if arrays are empty then if true then automatically set falue to false so the code can run
-        // if(localSavedBtns != null) {
-        //     inLocalArray = localSavedBtns.includes(response.name);
-        // }
-        // else  {
-        //     inLocalArray = false;
-        // }
 
-        // if(savedBtns != null) {
-        //     inArray = savedBtns.includes(response.name);
-        // }
-        // else {
-        //     inArray = false;
-        // }
 
-        
-
-        // if(inLocalArray) {
-        //     console.log("already in local array");
-        // }
-        // else if(inArray) {
-        //     console.log("already in array");
-        // }
-        // else {
-        //     if(savedBtns != null) {
-        //         savedBtns.push(response.name);
-        //         localStorage.setItem("savedCitys", JSON.stringify(savedBtns));
-        //     } else {
-        //         console.log("saved buttons is empty");
-        //     }
-        // }
+        if (savedBtns != null) {
+            savedBtns.push(response.name);
+            localStorage.setItem("savedCitys", JSON.stringify(savedBtns));
+        } else {
+            console.log("saved buttons is empty");
+        }
 
         // all the current weather info function
         currentWeatherInfo(response);
@@ -100,13 +74,12 @@ function currentWeatherInfo(response) {
         var getUvIndex = uvResponse[0].value;
 
         $("#UVindex").empty();
-        $("#UVindex").prepend("UV index: " + getUvIndex);
-
-
+        $("#UVindex").append("UV index: " + getUvIndex);
     });
 
 
     var fiveDayQueryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + key;
+
 
     $.ajax({
         url: fiveDayQueryURL,
@@ -114,16 +87,22 @@ function currentWeatherInfo(response) {
     }).then(function (fiveDayResponse) {
         console.log(fiveDayResponse);
 
+        $("#fiveDayBox").empty();
 
-        for(var i = 0; i < 5; i++) {
-            var fiveDates = moment().add(1+i, 'day').format('L');
+        var newTitle = $("<h3>");
+        newTitle.append("Five Day Forcast")
+        newTitle.attr("id", "fiveDayForcastTitle");
+        $("#fiveDayBox").append(newTitle);
+
+        for (var i = 0; i < 5; i++) {
+            var fiveDates = moment().add(1 + i, 'day').format('L');
             var datePar = $("<p>");
             datePar.css("font-weight", "bolder");
             datePar.css("font-size", "20px");
             datePar.append(fiveDates);
 
             var fiveIconURL = "http://openweathermap.org/img/wn/" + fiveDayResponse.list[i].weather[0].icon + ".png";
-            var iconImg = $("<image>");
+            var iconImg = $("<img>");
             iconImg.attr("src", fiveIconURL);
 
 
@@ -132,10 +111,12 @@ function currentWeatherInfo(response) {
             var tempPar = $("<p>");
             tempPar.css("font-size", "18px");
             tempPar.css("color", "black");
+            tempPar.css("text-align", "left");
             tempPar.append(getTempX5 + " F");
 
             var getHumidityX5 = fiveDayResponse.list[i].main.humidity;
             var humidityPar = $("<p>");
+            humidityPar.css("text-align", "left");
             humidityPar.append("Humidity: " + getHumidityX5 + "%");
 
 
@@ -151,7 +132,7 @@ function currentWeatherInfo(response) {
 
 
 
-            
+
 
         }
     });
@@ -167,19 +148,29 @@ function appendBtns(savedBtns, localSavedBtns) {
 
 
         for (var i = 0; i < localSavedBtns.length; i++) {
-            savedBtns.push(localSavedBtns[i]);
+            if(!savedBtns.includes(localSavedBtns[i])) {
+                savedBtns.push(localSavedBtns[i]);
+            }
+            else {
+                console.log("saved buttons array already contains that city");
+            }
         }
 
 
         console.log(savedBtns);
         for (var i = 0; i < savedBtns.length; i++) {
+            console.log(savedBtns);
             var newBtn = $("<button>");
-            newBtn.attr("data-name", savedBtns[i])
+            newBtn.attr("data-name", savedBtns[i]);
             newBtn.append(savedBtns[i]);
             newBtn.addClass("savedBtnStyle");
-            $("#newBtnSearch").append(newBtn);
+            $("#newBtnSearch").prepend(newBtn);
 
             savedBtnClicked(newBtn);
+        }
+
+        function updateButton(newBtn) {
+            $(".searchBtn").append(newBtn);
         }
     }
     else {
@@ -187,9 +178,9 @@ function appendBtns(savedBtns, localSavedBtns) {
     }
 }
 
-function savedBtnClicked(newBtn) {
+function savedBtnClicked(newBtn, city) {
     $(newBtn).click(function () {
-        // var getSavedBtnVal = $(this).attr("data-name");
-        // generateAllWeather(currentWeatherURL, localSavedBtns, savedBtns)
+        var getSavedBtnVal = $(this).attr("data-name");
+        city.val(getSavedBtnVal);
     })
 }
